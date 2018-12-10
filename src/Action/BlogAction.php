8da0a7a742a1ad23f337b1689db7de12dbc852d8
Vlad <vlad@nesutulsa.ru>
2018-12-10 15:37:48 +0300

    0.0.2 test

--- src/Action/BlogAction.php
+++ src/Action/BlogAction.php
@@ -16,23 +16,21 @@ final class BlogAction {
 	}
 
 	public function __invoke( ServerRequestInterface $request ) {
-		$totalPosts = count(Post::all());
+		$totalPosts = count( Post::all() );
 
-		$page = $request->getQueryParams()['page'] ?? 1;
+		$page         = $request->getQueryParams()['page'] ?? 1;
 		$postsPerPage = 3;
-		$offset = $page == 1 ? 0 : ($page-1) * $postsPerPage ;
+		$offset       = $page == 1 ? 0 : ( $page - 1 ) * $postsPerPage;
 
 		$posts = new Paginator(
-			Post::skip($offset)->take($postsPerPage)->get(),
+			Post::skip( $offset )->take( $postsPerPage )->get(),
 			$postsPerPage,
 			$page
 		);
 
-
-
 		return $this->renderer->make( 'blog-right', [
-			'title' => 'Блог',
-			'posts' => $posts,
+			'title'      => 'Блог',
+			'posts'      => $posts,
 			'totalPosts' => $totalPosts
 		] );
 	}
