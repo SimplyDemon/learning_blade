cd25bce89abd16f28237698c6d761f2d9cec6877
Vlad <vlad@nesutulsa.ru>
2018-12-13 17:18:21 +0300

    don't use env func too often

--- config/globals.php
+++ config/globals.php
@@ -1,6 +1,6 @@
 <?php
 define( 'HOME_URL', env('HOME_URL') );
 define( 'HOME_DIR', env('HOME_DIR'));
-define( 'CSS_URL', env('HOME_URL') . 'resources/css/' );
-define( 'JS_URL', env('HOME_URL') . 'resources/js/' );
-define( 'IMAGES_URL', env('HOME_URL') . 'resources/images/' );
+define( 'CSS_URL',HOME_URL . 'resources/css/' );
+define( 'JS_URL', HOME_URL . 'resources/js/' );
+define( 'IMAGES_URL',HOME_URL . 'resources/images/' );
