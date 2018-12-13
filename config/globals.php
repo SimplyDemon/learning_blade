3549755a6a4528c733baee694f367d45073cebb3
Vlad <vlad@nesutulsa.ru>
2018-12-13 09:23:33 +0300

    delete some env values

--- config/globals.php
+++ config/globals.php
@@ -1,8 +1,6 @@
 <?php
 define( 'HOME_URL', env('HOME_URL') );
 define( 'HOME_DIR', env('HOME_DIR'));
-define( 'ROUTE_PATH', env('ROUTE_PATH') );
-define( 'RESORCES_PATH', env('RESORCES_PATH') );
 define( 'CSS_URL', env('HOME_URL') . 'resources/css/' );
 define( 'JS_URL', env('HOME_URL') . 'resources/js/' );
-define( 'IMAGES_URL', env('IMAGES_URL') . 'resources/images/' );
+define( 'IMAGES_URL', env('HOME_URL') . 'resources/images/' );
